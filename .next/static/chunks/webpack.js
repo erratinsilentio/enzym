10cb0c9483b6138de2504f669dc35a1bc21ec912
erratinsilentio <kacper.kondracki@wp.pl>
2023-01-16 23:50:49 +0100

    created remaining routes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "269034f9234f86e9"; }
+/******/ 		__webpack_require__.h = function() { return "feeeab19dc1ca00c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
